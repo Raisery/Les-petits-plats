9ea4c78f74fb2f9afe3fbb019ee1b1743932037a
Lucas Gérard <lucas1.gerard@gmail.com>
2022-05-12 14:04:03 +0200

    gestions des filters

--- javascript/utils/UI.js
+++ javascript/utils/UI.js
@@ -28,6 +28,40 @@ class UI {
 
                     }
                     elt.textContent = content;
+
+                    elt.onclick = function(e) {
+                        const filtersContainer = document.querySelector(".filter__list");
+                        var alreadyIn = false;
+
+                        filtersContainer.childNodes.forEach(function(f) {
+                            if(f.childNodes[0].textContent == elt.textContent) {
+                                alreadyIn = true;
+                            }
+                        });
+
+                        if(alreadyIn) {
+                            return
+                        }
+
+                        const filter = document.createElement("div");
+                        filter.classList.add(`filter__list__item`);
+                        filter.classList.add(`filter__list__item--filter-type${selector.type}`);
+                        const name = document.createElement("p");
+                        name.classList.add("filter__list__item__name");
+                        name.textContent = elt.textContent;
+                        const cross = document.createElement("i");
+                        cross.classList.add("far");
+                        cross.classList.add("fa-times-circle");
+
+                        filtersContainer.appendChild(filter);
+
+                        filter.appendChild(name);
+                        filter.appendChild(cross);
+
+                        cross.onclick = function(e) {
+                            filtersContainer.removeChild(filter);
+                        }
+                    }
                     selector.html.childNodes[5].appendChild(elt);
                 }
                 nbItems++;
